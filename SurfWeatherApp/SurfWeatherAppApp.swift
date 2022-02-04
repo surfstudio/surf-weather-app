e968c0019800b41824541023efa1d36873eaf792
Vladislav Klimov <klimov-vd@yandex.ru>
2022-02-04 18:04:25 +0300

    Initial Commit

---
+++ SurfWeatherApp/SurfWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SurfWeatherAppApp.swift
+//  SurfWeatherApp
+//
+//  Created by Владислав Климов on 04.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct SurfWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
