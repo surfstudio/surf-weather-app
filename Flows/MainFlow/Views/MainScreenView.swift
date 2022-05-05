6bdb54f3636251cac4d03fec657e2aa9035ec4b8
porohov <kombat_kos@mail.ru>
2022-05-05 14:46:31 +0400

    Added shimmering

--- Flows/MainFlow/Views/MainScreenView.swift
+++ Flows/MainFlow/Views/MainScreenView.swift
@@ -11,13 +11,27 @@ struct MainScreenView: View {
     var body: some View {
         ScrollView {
             LocationHeaderView(viewModel: LocationHeaderViewModel())
-            Rectangle()
-                .frame(height: 392, alignment: .top)
-                .foregroundColor(.gray)
+            makeRactangle().padding(.all, 12)
             MainScreenForecastView(viewModel: .init())
             MainScreenForecastJournalView()
         }
         .background(Color.lightBackground | .darkBackground)
+    }
+
+    func makeRactangle() -> some View {
+
+        let shimmeringConfig = getShimmerConfig(animation: true)
+        return Rectangle()
+            .frame(height: 392, alignment: .top)
+            .foregroundColor(.gray)
+            .shimmer(isActive: true)
+            .environmentObject(shimmeringConfig)
+    }
+
+    func getShimmerConfig(animation: Bool) -> ShimmerConfig {
+        let config = ShimmerConfig(bgColor: Color.clear, fgColor: Color.clear)
+        if animation { config.startAnimation() }
+        return config
     }
 }
 
