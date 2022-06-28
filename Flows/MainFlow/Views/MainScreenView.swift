68f5aae1cfd1c954f5d141691aa3c75c2714845a
Konstantin Porokhov <56546669+kombatkos@users.noreply.github.com>
2022-06-28 12:56:10 +0400

    Lesson 3 Layout (#3)
    * Lesson3 Layout
    
    * added ScalePageView
    
    * Created CarouselView
    
    * Created scroll indicator
    
    * Configure ScrollIndicatorView
    
    * Configure update carousel
    
    * Deleted extra
    
    * Added marks
    
    * Edited ScrollDelegate
    
    * Edited width scale
    
    * Replace views
    
    * Replace Styles
    
    * Delete Api.xcconfig
    
    * Rename Api.xcconfig
    
    * Added Api.xcconfig gitIgnore

--- Flows/MainFlow/Views/MainScreenView.swift
+++ Flows/MainFlow/Views/MainScreenView.swift
@@ -13,18 +13,41 @@ struct MainScreenView: View {
 
     let viewModel: MainScreenViewModel
 
+    // MARK: - Private Properties
+
+    @State private var carouselMode: CardView.Mode = .long
+    @ObservedObject var carouselViewModel = CarouselViewModel()
+
     // MARK: - Views
 
     var body: some View {
         ScrollView {
-            LocationHeaderView(viewModel: viewModel.locationViewModel)
-            Rectangle()
-                .frame(height: 392, alignment: .top)
-                .foregroundColor(.gray)
+            LocationHeaderView(viewModel: LocationHeaderViewModel())
+            GeometryReader { makeCarousel(with: $0) }.frame(height: carouselMode == .short ? 188 : 360)
             MainScreenForecastView(viewModel: viewModel.forecastViewModel)
             MainScreenForecastJournalView()
         }
         .background(Color.lightBackground | .darkBackground)
+        .animation(.easeInOut(duration: 0.5), value: carouselMode)
+    }
+
+    func makeCarousel(with proxy: GeometryProxy) -> some View {
+        let topSpace = 80.0
+        let offset = proxy.frame(in: .global).minY - topSpace
+        DispatchQueue.main.async {
+            if carouselMode == .long && offset < .zero {
+                self.carouselMode = .short
+                self.carouselViewModel.updateIsNeeded = true
+            } else if carouselMode == .short && offset > .zero {
+                self.carouselMode = .long
+                self.carouselViewModel.updateIsNeeded = true
+            }
+        }
+        return CarouselView(
+            cardMode: $carouselMode,
+            updateIsNeeded: $carouselViewModel.updateIsNeeded,
+            viewModel: carouselViewModel
+        )
     }
 }
 
